3a8792d25bd8d6b4adb59d1f85156fbf96021cc1
Kevin Hu <hu.kevin.w@gmail.com>
2018-05-15 12:54:46 -0400

    Finishe login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+  }
+  onChange = ( e ) => {
+    this.setState({ [e.target.name] : e.target.value})
+  }
+  onSubmit = ( e ) => {
+    e.preventDefault(); 
+    const copyState = {...this.state}
+    console.log(copyState);
+  }
   render() {
     return (
-      <div>
-        <h1> login </h1>
+      <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <p className="lead text-center">Sign in to your DevConnector account</p>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" onChange={this.onChange}/>
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" onChange={this.onChange}/>
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
       </div>
+    </div>
     )
   }
 }
