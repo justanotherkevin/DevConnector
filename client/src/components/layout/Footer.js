1a3147c71563dc6caebc158ec69420c3fdc007df
Kevin Hu <hu.kevin.w@gmail.com>
2018-05-15 12:57:01 -0400

    formating

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,7 +5,6 @@ export default () => {
   // mt = margin top
   // p = padding 
   return (
-
     <footer className="bg-dark text-white mt-5 p-4 text-center">
       Copyright &copy; { new Date().getFullYear() } DevConnector
     </footer>
