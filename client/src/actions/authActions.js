8932bec35ed9b7b0191f4b29a922b9eed15716fe
Kevin Hu <hu.kevin.w@gmail.com>
2018-12-08 22:19:09 -0500

    Authorization actions

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,8 +1,60 @@
-import { TEST_DISPATCH } from './types';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import axios from 'axios';
+// due to nature of api request being async... you use the thunk middleware
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 // REGISTER USER
-export const registerUser = (userData) => {
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+      // this.setState({ errors: err.response.data });
+      // Object.keys(err.response.data).forEach( (key) => {
+      //   addClass( this.refs.registerForm[key], 'is-invalid' )
+      // })
+    });
+};
+
+// Login - get uer token
+// this is going to set the token into localstorage, then user the token in localstorage to call into private routes
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      console.log(res);
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      // token need to be decoded before use
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+// set logged in user
+export const setCurrentUser = decoded => {
   return {
-    type: TEST_DISPATCH,
-    payload:userData
-  }
-}
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+// log user out
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem('jwtToken');
+  //remove auth header from future request
+  setAuthToken(false);
+  // set current user to null which will set isAuthenticated to false
+  dispatch(setCurrentUser(null));
+};
