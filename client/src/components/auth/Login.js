0749a10c5f0883dbbd9ed3ade931b430654d6445
Kevin Hu <hu.kevin.w@gmail.com>
2018-12-08 22:25:34 -0500

    Create login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
   constructor() {
@@ -9,37 +12,95 @@ class Login extends Component {
       errors: {}
     };
   }
-  onChange = ( e ) => {
-    this.setState({ [e.target.name] : e.target.value})
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    // const copyState = { ...this.state };
+    // console.log(copyState);
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    this.props.loginUser(userData);
+  };
+  componentDidMount() {
+    // if user is isAuthenticated then redirect user to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
   }
-  onSubmit = ( e ) => {
-    e.preventDefault(); 
-    const copyState = {...this.state}
-    console.log(copyState);
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashbord');
+    }
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 m-auto">
-            <h1 className="display-4 text-center">Log In</h1>
-            <p className="lead text-center">Sign in to your DevConnector account</p>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" onChange={this.onChange}/>
-              </div>
-              <div className="form-group">
-                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" onChange={this.onChange}/>
-              </div>
-              <input type="submit" className="btn btn-info btn-block mt-4" />
-            </form>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={`
+                      form-control form-control-lg
+                      ${errors.email ? ' is-invalid' : ''}
+                    `}
+                    placeholder="Email Address"
+                    name="email"
+                    onChange={this.onChange}
+                  />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={`
+                      form-control form-control-lg 
+                      ${errors.password ? ' is-invalid' : ''}
+                    `}
+                    placeholder="Password"
+                    name="password"
+                    onChange={this.onChange}
+                  />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
-
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  // state.auth come from combineReducers @ index.js reducer
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
