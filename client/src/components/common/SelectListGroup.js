3f609a82f17d988642d9ec48473ce346a2aceb74
Kevin Hu <hu.kevin.w@gmail.com>
2018-12-11 11:41:04 -0500

    Create common input type components

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SelectListGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  info,
+  onChange,
+  options
+}) => {
+  const selectOptions = options.map(option => (
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </option>
+  ));
+  return (
+    <div className="form-group">
+      <select
+        className={`form-control form-control-lg ${error ? 'is-invalid' : ''}`}
+        name={name}
+        value={value}
+        onChange={onChange}
+      >
+        {selectOptions}
+      </select>
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
+};
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
+
+export default SelectListGroup;
