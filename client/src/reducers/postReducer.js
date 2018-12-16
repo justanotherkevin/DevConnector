2787531f8718b0782b8a5b4757669c53d5fe5617
Kevin Hu <hu.kevin.w@gmail.com>
2018-12-15 20:08:20 -0500

    Add post reducers

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,41 @@
+import {
+  ADD_POST,
+  GET_POSTS,
+  GET_POST,
+  DELETE_POST,
+  POST_LOADING
+} from '../actions/types';
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
