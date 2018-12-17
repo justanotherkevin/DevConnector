cf0160e6b758f39cf87adbfe281d4c17ad4359c6
Kevin Hu <hu.kevin.w@gmail.com>
2018-12-17 13:00:30 -0500

    Depolyment

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profiles");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
 
 const app = express();
 // Body parser middleware
@@ -13,23 +14,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true } )
-  .then(() => console.log("mongodb connected!!"))
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('mongodb connected!!'))
   .catch(err => console.log(err));
 
 // passport middleware
 app.use(passport.initialize());
-// Passport config 
-require("./config/passport")(passport);
+// Passport config
+require('./config/passport')(passport);
 
 // Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
+// server static assets if in production
+// because app contain both server and client, this dic will be hosted, and the client app need to be pointed to by the server, ( server is being run and hosted) the client app is being build by the server
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  // for any route, look in in the index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
